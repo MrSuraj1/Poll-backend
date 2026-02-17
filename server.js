6cc64c3b9121b8a2ed4f1f7531a2b7b3d5e257e0
MrSuraj1 <surajyadavsy271@gmail.com>
2026-02-17 18:00:33 +0530

    first commit

--- server.js
+++ server.js
@@ -3,7 +3,7 @@ const express = require("express");
 const http = require("http");
 const { Server } = require("socket.io");
 const cors = require("cors");
-const pollRoutes = require("./Routes/pollroute");
+const pollRoutes = require("./Routes/routePol");
 
 const app = express();
 const server = http.createServer(app);
